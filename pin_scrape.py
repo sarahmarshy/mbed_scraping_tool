b4392ed9b686f29269392b0b7f3dfa47d808b401
sarahmarshy <sarah.marsh@arm.com>
2015-06-29 12:51:39 -0500

    Defines method that will return dictionaries of the form {pin_type:list of tuples in the form of (pin name, pin location, pin channel)}. The function takes a filename and line numbers where PinMaps are declared.

---
+++ pin_scrape.py
@@ -0,0 +1,24 @@
+__author__ = 'sarmar01'
+import re
+
+def extract_pins(lines, lineno):
+    for i,line in enumerate(lines[lineno:]):
+        if ";" in line:
+            return
+        else:
+            regex = "\s*{(\w+)\s*,\s*(\w+)\s*,\s*(\d+)}\s*,"
+            m = re.search(regex,line)
+            if m:
+                yield (m.group(1),m.group(2),m.group(3))
+
+def find_pinmaps(filename,linenos):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        for lineno in linenos:
+            regex = r"PinMap PinMap_(\w+)\[\]"
+            m = re.search(regex, lines[lineno-1])
+            if m:
+                type = m.group(1)
+                yield {type:extract_pins(lines,lineno-1)}
+
+
